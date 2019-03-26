6ce54689411e18106a60e3c75fb48ea14be55028
Celia Sherry <sherry.c@husky.neu.edu>
2019-03-25 20:36:40 -0400

    nav bar started

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import ArticleService from "../services/ArticleService";
+import TopNav from "./TopNav";
 
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.articleService = ArticleService.getInstance();
+        this.state = {
+            articles: []
+        }
     }
+
+    componentDidMount = () =>
+        this.findAllArticles();
+
+    componentDidUpdate = () =>
+        this.findAllArticles();
+
+    findAllArticles = () =>
+        this.articleService.findAllArticles()
+            .then(articles =>
+            this.setState ({
+                articles: articles
+            }))
 
     render() {
         return (
+            <div>
+                <TopNav/>
             <div className="container-fluid">
                 <h1>
                     Home Page
                 </h1>
+            </div>
             </div>
         )
     }
---
+++ src/components/TopNav.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+const TopNav = ({}) =>
+    <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <button className="navbar-toggler" type="button" data-toggle="collapse">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse">
+                <a className="navbar-brand" href="#">
+                    Health Portal
+                </a>
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            Profile
+                        </a>
+                    </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0 d-sm-none d-md-block">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-success my-2 my-sm-0" type="submit">
+                            Search
+                        </button>
+                </form>
+            </div>
+        </nav>
+
+    </div>
+export default TopNav;
