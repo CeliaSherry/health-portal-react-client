5fdbc1d92542b8978ae86f15d9d32d5b9f7a2a30
Celia Sherry <sherry.c@husky.neu.edu>
2019-03-25 20:39:45 -0400

    adding css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import ArticleService from "../services/ArticleService";
 import TopNav from "./TopNav";
+import './Home.css';
 
 class Home extends Component {
     constructor(props) {
